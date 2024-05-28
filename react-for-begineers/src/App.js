1a08e78c6d78b256fea409cafbf732be68a54051
31daylee <gussla2206@gmail.com>
2024-05-28 16:26:48 +0900

    style add

--- react-for-begineers/src/App.js
+++ react-for-begineers/src/App.js
@@ -2,20 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 import "./styles.css";
-import { useState } from "react";
-import Aside from "./components/Aside";
 
 function App() {
-  const [genres, setGenres] = useState([
-    "Action",
-    "Comedy",
-    "Romance",
-    "Horror",
-  ]);
-
   return (
     <Router>
-      <Aside width={320} genres={genres} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<Detail />} />
