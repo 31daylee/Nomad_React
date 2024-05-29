07313df56c37870923be8529622634924463b89e
31daylee <gussla2206@gmail.com>
2024-05-29 10:28:30 +0900

    1.1 useInput

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 const useInput = (initialValue) => {
   const [value, setValue] = useState(initialValue);
+  const onChange = (e) => {
+    console.log(e.target);
+  };
+  return { value, onChange };
 };
 
 const App = () => {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+  const name = useInput("Mr.");
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>increment</button>
-      <button onClick={decrementItem}>decrement</button>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name}></input>
     </div>
   );
 };
