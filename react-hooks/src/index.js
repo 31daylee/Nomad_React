d4f3f0c0dddd50cf6b51bab6650178e9465dd28b
31daylee <gussla2206@gmail.com>
2024-05-29 14:46:04 +0900

    2.1 useTitle

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,34 +1,21 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
-const content = [
-  {
-    tab: "Section 1",
-    content: "I'm the content of the Section 1",
-  },
-  {
-    tab: "Section 2",
-    content: "I'm the content of the Section 2",
-  },
-];
-const useTabs = (initialTab, allTabs) => {
-  const [currentIndex, setCurrentIndex] = useState(initialTab);
-  if (!allTabs || !Array.isArray(allTabs)) {
-    return;
-  }
-
-  return {};
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
 };
 const App = () => {
-  const sayHello = () => console.log("Hello");
-  const [number, setNumber] = useState(0);
-  const [aNumber, setAnumber] = useState(0);
-  useEffect(sayHello, [number]);
+  const titleUpdater = useTitle("Loading..."); // useTitle에서 setTitle()이란 함수를 return 해주고 있음
+  setTimeout(() => titleUpdater("Home"), 5000);
   return (
     <div className="App">
       <h1>Hi</h1>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
 };
