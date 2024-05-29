7a0f27492bc185901350daecefa6e9565dd04cf6
31daylee <gussla2206@gmail.com>
2024-05-30 08:55:22 +0900

    2.2 useClick

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,21 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  };
-  useEffect(updateTitle, [title]);
-  return setTitle;
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (typeof onClick !== "function") {
+      return;
+    }
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return typeof onClick !== "function" ? undefined : element;
 };
 const App = () => {
-  const titleUpdater = useTitle("Loading..."); // useTitle에서 setTitle()이란 함수를 return 해주고 있음
-  setTimeout(() => titleUpdater("Home"), 5000);
+  const sayHello = () => console.log("say Hello");
+  const title = useClick(sayHello);
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <h1 ref={title}>Hi</h1>
     </div>
   );
 };
