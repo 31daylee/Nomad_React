86d3c6eb1444764f34c4887f1ec45802cd49e74d
31daylee <gussla2206@gmail.com>
2024-05-31 15:58:20 +0900

    2.7 useNotification

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,8 +1,34 @@
 import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
 
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
+    }
+  };
+  return fireNotif;
+};
 const App = () => {
-  return <div className="App"></div>;
+  const triggerNotif = useNotification("Can I steal your account?", {
+    body: "please",
+  });
+  return (
+    <div className="App">
+      <button onClick={triggerNotif}>Hello</button>
+    </div>
+  );
 };
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
